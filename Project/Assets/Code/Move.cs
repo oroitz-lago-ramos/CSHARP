2d9813ff9e6f849da81732465e83acbaaf2c6d06
oroitz-lago-ramos <oroitz.lago-ramos@laplateforme.io>
2024-09-25 16:57:34 +0200

    `Anthony` -- Added checks for arrow keys for movements.

--- Project/Assets/Code/Move.cs
+++ Project/Assets/Code/Move.cs
@@ -4,10 +4,10 @@ public class Move : MonoBehaviour{
     public float speed = 10f;
     public void FixedUpdate(){
         var movement = new Vector3();
-        if(Input.GetKey(KeyCode.W)){movement += Vector3.up;}
-        if(Input.GetKey(KeyCode.S)){movement += -Vector3.up;}
-        if(Input.GetKey(KeyCode.A)){movement += -Vector3.right;}
-        if(Input.GetKey(KeyCode.D)){movement += Vector3.right;}
+        if(Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)) {movement += Vector3.up;}
+        if(Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow)){movement += -Vector3.up;}
+        if(Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)){movement += -Vector3.right;}
+        if(Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) {movement += Vector3.right;}
         this.rigidBody.linearVelocity = movement.normalized * this.speed * Time.fixedDeltaTime;
     }
 }
