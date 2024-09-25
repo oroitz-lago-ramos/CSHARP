1cfa1702395316a1d2e88e2a6e3e741676314f56
oroitz-lago-ramos <oroitz.lago-ramos@laplateforme.io>
2024-09-25 15:45:05 +0200

    Syncing changes from master

---
+++ Project/Assets/Inventory.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+public enum 
+public class Item{
+    public string name;
+    public 
+}
+public class Inventory : MonoBehaviour{
+    public List<Item> items;
+    public void Start(){
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
