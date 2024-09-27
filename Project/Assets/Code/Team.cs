5ae50d563b60aea51b171988aa1cb104c9885174
OroitzLR <oroitzlr@gmail.com>
2024-09-27 21:43:25 +0200

    <feat>[INGAMEMENU]:<added inGameMenu into the map scene, it stills one bug remain that unity don't detect the input>

--- Project/Assets/Code/Team.cs
+++ Project/Assets/Code/Team.cs
@@ -1,4 +1,18 @@
 using UnityEngine;
 public class Team : MonoBehaviour{
     public Entity[] members;
+    public static Team main;
+
+    private void Awake()
+    {
+        if (main == null)
+        {
+            main = this;
+        }
+        else if (main != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
 }
