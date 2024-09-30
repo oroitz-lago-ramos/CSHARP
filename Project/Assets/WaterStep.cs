10a9847890f2fb65d2cbe5e90163813b35034ce0
OroitzLR <oroitzlr@gmail.com>
2024-09-30 14:57:35 +0200

    <feat>[MAP]:<added splash sound effect on mini lakes>

---
+++ Project/Assets/WaterStep.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class WaterStep : MonoBehaviour
+{
+    public new AudioSource audio;
+    public LayerMask characterLayer;
+   
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+           
+    }
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        var layer = 1 << collision.gameObject.layer;
+        if ((layer & this.characterLayer) != 0)
+        {
+            Debug.Log("Ici");
+            audio.Play();
+        }
+    }
+}
