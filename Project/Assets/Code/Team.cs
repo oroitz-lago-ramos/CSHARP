a5c416c036036915c4ad2d5fc7650fbe8b00befc
OroitzLR <oroitzlr@gmail.com>
2024-09-30 16:13:04 +0200

    MERGE 2

--- Project/Assets/Code/Team.cs
+++ Project/Assets/Code/Team.cs
@@ -1,4 +1,28 @@
 using UnityEngine;
 public class Team : MonoBehaviour{
     public Entity[] members;
+    public static Team main;
+
+    private void Awake()
+    {
+        if (main == null)
+        {
+            main = this;
+        }
+        else if (main != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void Swap(int index)
+    {
+        if (index < members.Length)
+        {
+            Entity temp = members[0];
+            members[0] = members[index];
+            members[index] = temp;
+        }
+    }
+
 }
