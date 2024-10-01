78d43ef929c2663a8077ecf84f499cbef3a70f55
oroitz-lago-ramos <oroitz.lago-ramos@laplateforme.io>
2024-10-01 16:02:19 +0200

    -- Renamed `AudioSource` to avoid inheritance name conflict.

--- Project/Assets/Code/Music.cs
+++ Project/Assets/Code/Music.cs
@@ -3,15 +3,15 @@ using UnityEngine;
 public class Music : MonoBehaviour
 {
     public LayerMask characterLayer;
-    public AudioSource audio;
+    public AudioSource source;
     public AudioClip clip;
     public void OnTriggerEnter2D(Collider2D collision)
     {
         var layer = 1 << collision.gameObject.layer;
         if ((layer & this.characterLayer) != 0)
         {
-            audio.clip = clip;
-            audio.Play();
+            source.clip = clip;
+            source.Play();
         }
         
     }
